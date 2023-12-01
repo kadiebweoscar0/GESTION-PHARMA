295646f91d884e5ed9c5c8da792034b13d9471ab
Kadiebwe <oscar.kadiebwe@kadea.local>
2023-12-01 12:12:31 +0100

    using a hook useParams for clone a parameter of the url in singleProduct.jsx

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 
-
 const products = [
     {
         id: "1",
--- src/pages/singleProduct.jsx
+++ src/pages/singleProduct.jsx
@@ -1,9 +1,16 @@
-
+import ProductPreview from "../components/ProductPreview";
+import { useParams } from "react-router-dom";
 
 function SingleProduct() {
+    const useParam = useParams()
+    const {id} = useParam;
     return(
-        <h1>SingleProduct</h1>
-        
+        <>
+            <h1>SingleProduct</h1>
+            {id}
+            <ProductPreview />
+        </>
+
     )
 }
 export default SingleProduct;
