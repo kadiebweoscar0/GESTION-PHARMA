d165ccb2e180215ca6c43db13afb5941a8bce804
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-12-01 16:01:12 +0100

    add a functonalyti in singleProduct for display a description, image and name of the product onClick

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,27 @@ import SingleProduct from "./pages/singleProduct";
 import ProductsList from "./pages/ProductsList";
 import NavBar from "./components/navBar";
 
+const products = [
+  {
+      id: "1",
+      name: "Doliprane",
+      image: "https://dummyimage.com/200x200/000/fff&text=Doliprane",
+      description: "Le Doliprane est un médicament contre la douleur et la fièvre couramment utilisé pour soulager les maux de tête, les douleurs musculaires, les courbatures, les douleurs dentaires, les maux de gorge et les fièvres."
+  },
+  {
+      id: "2",
+      name: "Aspirine",
+      image: "https://dummyimage.com/200x200/000/fff&text=Aspirine",
+      description: "L'aspirine est un médicament anti-inflammatoire non stéroïdien (AINS) qui est utilisé pour soulager la douleur, la fièvre et l'inflammation."
+  },
+  {
+      id: "3",
+      name: "Ibuprofène",
+      image: "https://dummyimage.com/200x200/000/fff&text=Ibuprofene",
+      description: "Il est disponible en comprimés, en gélules, en suppositoires et en sirop. La dose recommandée pour les adultes est de 200 à 400 milligrammes par prise, jusqu'à 4 prises par jour"
+  }
+];
+
 function App() {
 
   return (
@@ -18,8 +39,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} >
-          <Route index element={<ProductsList />}/>
-          <Route path=":id" element={<SingleProduct />} />
+          <Route index element={<ProductsList newProduct={products} />}/>
+          <Route path=":id" element={<SingleProduct newProduct={products} />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/singleProduct.jsx
+++ src/pages/singleProduct.jsx
@@ -1,13 +1,23 @@
 import ProductPreview from "../components/ProductPreview";
 import { useParams } from "react-router-dom";
 
-function SingleProduct() {
+function SingleProduct({newProduct}) {
     const useParam = useParams()
     const {id} = useParam;
+
+    const medicament = newProduct.find((medic) => medic.id === id);
+
+    if (!medicament) {
+        return <p>L&lsquo;medicament que vous avez demander n&lsquo;existe pas</p>;
+    }
+
+
     return(
         <>
             <h1>SingleProduct</h1>
-            {id}
+            <h2>{medicament.name}  {id}</h2>
+            <img src={medicament.image} alt="photo-medicament" />
+            <h4>{medicament.description}</h4>
             <ProductPreview />
         </>
 
